35e22041daeb4b351ab1d63a3b4cfb0ef52b42cf
glherlngg <galih141105erlangga@gmail.com>
2024-07-06 19:31:16 +0700

    menambahkan halaman login

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+include('koneksi.php');
+if (isset($_POST['submit']))
+{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM tb_user WHERE email='$email'AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0)
+    {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['username'] = $row['nama_lengkap'];
+        header("Location: dashboard.php");
+        exit();
+    }else{
+        echo "<script>alert('Email atau password Anda salah. Silakan coba lagi')</script>";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en"> <!--begin::Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Aplikasi | Sederhana</title><!--begin::Primary Meta Tags-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="AdminLTE 4 | Login Page">
+    <meta name="author" content="ColorlibHQ">
+    <meta name="description" content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
+    <meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard"><!--end::Primary Meta Tags--><!--begin::Fonts-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous"><!--end::Fonts--><!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css" integrity="sha256-dSokZseQNT08wYEWiz5iLI8QPlKxG+TswNRD8k35cpg=" crossorigin="anonymous"><!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Third Party Plugin(Bootstrap Icons)-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
+    <link rel="stylesheet" href="adminlte.css"><!--end::Required Plugin(AdminLTE)-->
+</head> <!--end::Head--> <!--begin::Body-->
+
+<body class="login-page bg-body-secondary">
+    <div class="login-box">
+        <div class="login-logo"> <a href="../index2.html"><b>POLTEK</b>AGA</a> </div> <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <form id="Login form" action="" method="post">
+                    <div class="input-group mb-3"> <input type="email" id="email" name="email"  class="form-control" placeholder="Email" required>
+                        <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                    </div>
+                    <div class="input-group mb-3"> <input type="password" id="password" name="password"class="form-control" placeholder="Password" required>
+                        <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                    </div> <!--begin::Row-->
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> <label class="form-check-label" for="flexCheckDefault">
+                                    Remember Me
+                                </label> </div>
+                        </div> <!-- /.col -->
+                        <div class="col-4">
+                            <div class="d-grid gap-2"> <button name="submit" type="submit" class="btn btn-primary">Sign In</button> </div>
+                        </div> <!-- /.col -->
+                    </div> <!--end::Row-->
+                </form>
+              
+               
+            </div> <!-- /.login-card-body -->
+        </div>
+    </div> <!-- /.login-box --> <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script> <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha256-whL0tQWoY1Ku1iskqPFvmZ+CHsvmRWx/PIoEvIeWh4I=" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
+    <script src="../../../dist/js/adminlte.js"></script> <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+    <script>
+        
+        const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
+        const Default = {
+            scrollbarTheme: "os-theme-light",
+            scrollbarAutoHide: "leave",
+            scrollbarClickScroll: true,
+        };
+        document.addEventListener("DOMContentLoaded", function() {
+            const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+            if (
+                sidebarWrapper &&
+                typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
+            ) {
+                OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+                    scrollbars: {
+                        theme: Default.scrollbarTheme,
+                        autoHide: Default.scrollbarAutoHide,
+                        clickScroll: Default.scrollbarClickScroll,
+                    },
+                });
+            }
+        });
+   
+     //function validateLogin(){
+      //  var Gmail=
+      //  document.getElementById("Gmail").value;
+      //  var password= 
+      //  document.getElementById("password").value;
+       // if (Gmail==="galih@gmail.com"&&password==="ganteng"){
+      //      return true;
+     //   }
+    //    else
+    //    {
+     //       window.alert('user dan password salah');
+    //        return false;
+     //   }
+ //   }
+</script> <!--end::Script--> 
+<!--end::OverlayScrollbars Configure-->
+</body><!--end::Body-->
+
+</html>
