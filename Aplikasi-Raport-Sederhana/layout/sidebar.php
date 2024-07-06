dc8191f0a56324baa3a6f849ad62ac5d4d3895bb
glherlngg <galih141105erlangga@gmail.com>
2024-07-06 19:57:10 +0700

    memperbarui side bar dan menambah logout

--- Aplikasi-Raport-Sederhana/layout/sidebar.php
+++ Aplikasi-Raport-Sederhana/layout/sidebar.php
@@ -1,70 +1,77 @@
- <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../../index.html">
-                <div class="sidebar-brand-icon ">
-                    <i class="fas fa-book-reader"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Akademik</div>
-            </a>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../../index.html">
+        <div class="sidebar-brand-icon ">
+            <i class="fas fa-book-reader"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Poltek AGA</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item <?php if($title == 'dashboard') echo "active" ?>">
-                <a class="nav-link" href="../index.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item <?php if($title == 'dashboard') echo "active" ?>">
+        <a class="nav-link" href="../dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Menu
-            </div>
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Menu
+    </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item <?php if($title == 'siswa') echo 'active' ?>">
-                <a class="nav-link" href="../siswa/siswa.php">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Siswa</span>
-                </a>
-            </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item <?php if($title == 'siswa') echo 'active' ?>">
+        <a class="nav-link" href="../siswa/siswa.php">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Mahasiswa</span>
+        </a>
+    </li>
 
-             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item <?php if($title == 'nilai') echo 'active' ?>">
-                <a class="nav-link" href="../nilai/nilai.php">
-                     <i class="fas fa-fw fa-graduation-cap"></i>
-                    <span>Nilai</span>
-                </a>
-            </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item <?php if($title == 'nilai') echo 'active' ?>">
+        <a class="nav-link" href="../nilai/nilai.php">
+            <i class="fas fa-fw fa-graduation-cap"></i>
+            <span>Nilai</span>
+        </a>
+    </li>
 
-              <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item <?php if($title == 'guru') echo 'active' ?>">
-                <a class="nav-link" href="../guru/guru.php">
-                    <i class="fas fa-fw fa-chalkboard-teacher"></i>
-                    <span>Guru</span>
-                </a>
-            </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item <?php if($title == 'guru') echo 'active' ?>">
+        <a class="nav-link" href="../guru/guru.php">
+            <i class="fas fa-fw fa-chalkboard-teacher"></i>
+            <span>Dosen</span>
+        </a>
+    </li>
 
-            <li class="nav-item <?php if($title == 'mata-pelajaran') echo 'active' ?>">
-                <a class="nav-link" href="../mata-pelajaran/mata-pelajaran.php">
-                    <i class="fas fa-fw fa-chalkboard"></i>
-                    <span>Mata Pelajaran</span>
-                </a>
-            </li>
+    <li class="nav-item <?php if($title == 'mata-pelajaran') echo 'active' ?>">
+        <a class="nav-link" href="../mata-pelajaran/mata-pelajaran.php">
+            <i class="fas fa-fw fa-chalkboard"></i>
+            <span>Mata Kuliah</span>
+        </a>
+    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+    <!-- Nav Item - Log Out -->
+    <li class="nav-item">
+        <a class="nav-link" href="../index.php">
+            <i class="fas fa-fw fa-sign-out-alt"></i>
+            <span>Log Out</span></a>
+    </li>
 
-        </ul>
-        <!-- End of Sidebar -->
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
