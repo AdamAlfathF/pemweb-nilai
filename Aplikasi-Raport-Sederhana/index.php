07ea4a566b024da52b29c32611c5e01b9162ba93
aishalunak <aishalunak@gmail.com>
2024-07-08 14:53:41 +0700

    ganti judul
    judul aplikasi

--- Aplikasi-Raport-Sederhana/index.php
+++ Aplikasi-Raport-Sederhana/index.php
@@ -27,7 +27,7 @@ if (isset($_POST['submit']))
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Aplikasi | Sederhana</title><!--begin::Primary Meta Tags-->
+    <title>Aplikasi | Nilai</title><!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="AdminLTE 4 | Login Page">
     <meta name="author" content="ColorlibHQ">
