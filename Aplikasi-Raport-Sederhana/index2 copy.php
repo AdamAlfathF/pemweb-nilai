7d2fe34575f02e1510440f7399a580026f061978
glherlngg <galih141105erlangga@gmail.com>
2024-07-13 21:27:08 +0700

    fix bug index dosen

--- Aplikasi-Raport-Sederhana/index2 copy.php
+++ Aplikasi-Raport-Sederhana/index2 copy.php
@@ -73,31 +73,32 @@ $title = 'dashboard';
                     <div class="row">
 
                        
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+ <!-- Earnings (Monthly) Card Example -->
+ <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Guru (Jumlah)</div>
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Mahasiswa (Jumlah)</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                             <?php
-                                                $dataguru = getList("SELECT count(nip) as jumlahGuru FROM guru");
-                                                 foreach ($dataguru as $row) {
-                                                        echo $row['jumlahGuru'];
+                                                $datasiswa = getList("SELECT count(nis) as jumlahSiswa FROM siswa");
+                                                 foreach ($datasiswa as $row) {
+                                                        echo $row['jumlahSiswa'];
                                                  }
                                              ?>
-                                            </div>
+
+                                        </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-chalkboard-teacher fa-2x text-gray-300"></i>
+                                            <i class="fas fa-users fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        
 
                           <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-warning shadow h-100 py-2">
