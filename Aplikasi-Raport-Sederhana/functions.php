ee6310b0593f34a85c6f5d92f8903a9e6ac6bfe4
glherlngg <galih141105erlangga@gmail.com>
2024-07-18 08:49:44 +0700

    memperbaiki fungsi hapus

--- Aplikasi-Raport-Sederhana/functions.php
+++ Aplikasi-Raport-Sederhana/functions.php
@@ -1,15 +1,14 @@
 <?php
-define("DEVELOPMENT",TRUE);
+define("DEVELOPMENT", TRUE);
+
 function dbConnect(){
-	$db=new mysqli("localhost","root","","raport");// Sesuaikan dengan konfigurasi server anda.
-	return $db;
+    $db = new mysqli("localhost", "root", "", "raport");
+    return $db;
 }
 
 $con = dbConnect();
 
-
-function bisa($con, $query)
-{
+function bisa($con, $query){
     $db = mysqli_query($con, $query);
 
     if ($db) {
@@ -19,79 +18,75 @@ function bisa($con, $query)
     }
 }
 
-function getListPelajaran()
-{
+function getListPelajaran(){
     $conn = dbConnect();
-    if($conn->connect_errno==0){
-		$res=$conn->query("SELECT * 
-						 FROM mata_pelajaran
-                         ORDER BY kd_mp");
-		if($res){
-			$data=$res->fetch_all(MYSQLI_ASSOC);
-			$res->free();
-			return $data;
-		}
-		else
-			return FALSE; 
-	}
-	else
-		return FALSE;
+    if ($conn->connect_errno == 0) {
+        $res = $conn->query("SELECT * FROM mata_pelajaran ORDER BY kd_mp");
+        if ($res) {
+            $data = $res->fetch_all(MYSQLI_ASSOC);
+            $res->free();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    } else {
+        return FALSE;
+    }
 }
 
-function getList($query)
-{
+function getList($query){
     $conn = dbConnect();
-    if($conn->connect_errno==0){
-		$res=$conn->query($query);
-		if($res){
-			$data=$res->fetch_all(MYSQLI_ASSOC);
-			$res->free();
-			return $data;
-		}
-		else
-			return FALSE; 
-	}
-	else
-		return FALSE;
+    if ($conn->connect_errno == 0) {
+        $res = $conn->query($query);
+        if ($res) {
+            $data = $res->fetch_all(MYSQLI_ASSOC);
+            $res->free();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    } else {
+        return FALSE;
+    }
 }
 
-
-function getListSiswa()
-{
+function getListSiswa(){
     $conn = dbConnect();
-    if($conn->connect_errno==0){
-		$res=$conn->query("SELECT * 
-						 FROM siswa
-                         ORDER BY nis");
-		if($res){
-			$data=$res->fetch_all(MYSQLI_ASSOC);
-			$res->free();
-			return $data;
-		}
-		else
-			return FALSE; 
-	}
-	else
-		return FALSE;
+    if ($conn->connect_errno == 0) {
+        $res = $conn->query("SELECT * FROM siswa ORDER BY nis");
+        if ($res) {
+            $data = $res->fetch_all(MYSQLI_ASSOC);
+            $res->free();
+            return $data;
+        } else {
+            return FALSE;
+        }
+    } else {
+        return FALSE;
+    }
 }
 
-function ambilsatubaris($conn, $query)
-{
+function ambilsatubaris($conn, $query){
     $db = mysqli_query($conn, $query);
     return mysqli_fetch_assoc($db);
 }
 
-function hapus($where, $table, $con)
-{
+function hapus($where, $table, $con){
     $query = 'DELETE FROM ' . $table . ' WHERE ' . $where;
-    echo $query;
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 function showError($message){
-	?>
-<div style="background-color:#FAEBD7;padding:10px;border:1px solid red;margin:15px 0px">
-<?php echo $message;?>
-</div>
-	<?php
+    ?>
+    <div style="background-color:#FAEBD7;padding:10px;border:1px solid red;margin:15px 0px">
+        <?php echo $message; ?>
+    </div>
+    <?php
 }
 ?>
--- Aplikasi-Raport-Sederhana/mata-pelajaran/mata-pelajaran-hapus.php
+++ Aplikasi-Raport-Sederhana/mata-pelajaran/mata-pelajaran-hapus.php
@@ -1,27 +1,32 @@
-<?php 
-	session_start();
- 
-	// cek apakah yang mengakses halaman ini sudah login
-	if($_SESSION['level']==""){
-		header("location:../index.php?pesan=gagal");
-	}
- 
-	?>
-
-
-
 <?php
+session_start();
+
+// cek apakah yang mengakses halaman ini sudah login
+if ($_SESSION['level'] == "") {
+    header("location:../index.php?pesan=gagal");
+}
+
 include_once("../functions.php");
+
+$con = dbConnect();
 
 $kd_mp = $_GET['kd_mp'];
 
-$query = "DELETE FROM mata_pelajaran WHERE kd_mp = $kd_mp";
+// Hapus data dari tabel nilai yang memiliki foreign key terkait
+$query_nilai = "DELETE FROM nilai WHERE kd_mp = '$kd_mp'";
+$execute_nilai = bisa($con, $query_nilai);
 
-$execute = bisa($con, $query);
+// Hapus data dari tabel guru yang memiliki foreign key terkait
+$query_guru = "DELETE FROM guru WHERE kd_mp = '$kd_mp'";
+$execute_guru = bisa($con, $query_guru);
 
-if($execute == 1){
+// Hapus data dari tabel mata_pelajaran setelah menghapus data terkait di tabel nilai dan guru
+$query_mata_pelajaran = "DELETE FROM mata_pelajaran WHERE kd_mp = '$kd_mp'";
+$execute_mata_pelajaran = bisa($con, $query_mata_pelajaran);
+
+if ($execute_mata_pelajaran == 1) {
     header('location: mata-pelajaran.php');
+} else {
+    echo "Gagal menghapus data";
 }
-else
-echo "Gagal menghapus data";
 ?>
