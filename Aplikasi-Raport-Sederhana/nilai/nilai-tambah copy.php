29ab5b3ab92494941a737aa4e824d02591efeabf
aishalunak <aishalunak@gmail.com>
2024-07-18 07:17:38 +0700

    Update nilai-tambah copy.php
    mengganti title

--- Aplikasi-Raport-Sederhana/nilai/nilai-tambah copy.php
+++ Aplikasi-Raport-Sederhana/nilai/nilai-tambah copy.php
@@ -98,7 +98,7 @@ if(isset($_POST["btn-simpan"])){
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Mahasiswa</title>
+    <title>Nilai Mahasiswa</title>
 
     <!-- Custom fonts for this template-->
     <link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
